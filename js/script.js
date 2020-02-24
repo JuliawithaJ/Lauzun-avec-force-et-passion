137403fac3c0a3985f7c4d62f065dd407e45472e
JuliawithaJ <juls.cheroux@gmail.com>
2020-02-24 22:17:32 +0100

    script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-function myFunction() {
-  var popup = document.getElementById('myPopup');
-  popup.classList.toggle('show');
-}
 function evelyne() {
   var popup = document.getElementById('evelyne');
   popup.classList.toggle('show');
