b8e0ac3c86f51dee42ba76d416941c3c7b068c3f
JuliawithaJ <juls.cheroux@gmail.com>
2020-02-25 00:31:00 +0100

    JS

--- js/script.js
+++ js/script.js
@@ -60,14 +60,15 @@
 // }
 function remove_show() {
   let popups = document.getElementsByClassName('popup');
-  for (popup in popups) {
-    popup.classList.remove('show');
+  for (var i = 0; i < popups.length; i++) {
+    popups.item(i).children[0].classList.remove('show');
   }
 }
 let popups = document.getElementsByClassName('popup');
-for (popup in popups) {
-  popup.addEventListener('click', function() {
+for (var i = 0; i < popups.length; i++) {
+  let element = popups.item(i);
+  element.addEventListener('click', function() {
     remove_show();
-    this.firstChild.classList.add('show');
+    element.children[0].classList.remove('show');
   });
 }
